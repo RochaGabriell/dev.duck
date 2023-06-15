f02ce6b8d853b6e72891fa82882529ec14793d2f
RochaGabriell <rochagabriell.dev@gmail.com>
2023-06-15 10:24:10 -0300

    :construction: Work in progress.

---
+++ devduck/apps/core/models/__init__.py
@@ -0,0 +1,1 @@
+from .RequestPermissio import *
--- devduck/apps/core/views/PermissionView.py
+++ devduck/apps/core/views/PermissionView.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import View
+from django.views.generic import CreateView, ListView, View
 
 
 class RequestPermissionView(LoginRequiredMixin, View):
