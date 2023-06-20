6d13b57f2035117f9dcffa6d9281c17d87dd547f
RochaGabriell <rochagabriell.dev@gmail.com>
2023-06-20 15:33:02 -0300

    :bento: Add or update assets.

---
+++ devduck/base_static/global/javascript/see_post.js
@@ -0,0 +1,4 @@
+var markdown = document.getElementById("markdown").value;
+var converter = new showdown.Converter();
+var html = converter.makeHtml(markdown);
+document.getElementById("html").innerHTML = html;
