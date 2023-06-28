cfaaf7082e97b748aeaff0f5a8905bda06a8856a
RochaGabriell <rochagabriell.dev@gmail.com>
2023-06-28 01:11:13 -0300

    :iphone: Work on responsive design.

--- devduck/apps/core/forms/RequestForm.py
+++ devduck/apps/core/forms/RequestForm.py
@@ -11,8 +11,6 @@ class RequestForm(ModelForm):
     description = forms.CharField(
         widget=forms.Textarea(attrs={
             'class': 'description-request',
-            'cols': '40',
-            'rows': '25',
             'placeholder': 'Escreva o motivo da sua solicitação aqui...',
         })
     )
